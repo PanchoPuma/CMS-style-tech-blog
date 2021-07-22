8a01fbb00be3b70ab258a4544203d3eee606ca29
Francisco Puma <francis_pancho12@hotmail.com>
2021-07-21 22:18:23 -0700

    started work in controllers

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,27 +3,27 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Nunc purus.',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    content: 'Nunc purus',
     user_id: 4
   },
   {
     title: 'Pellentesque eget nunc.',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
+    content: 'Pellentesque eget nunc',
     user_id: 7
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    content: 'Morbi non quam nec dui luctus rutruml',
     user_id: 1
   },
   {
     title: 'Etiam justo.',
-    post_url: 'https://shareasale.com/quis.json',
+    content: 'Etiam justo',
     user_id: 4
   },
   {
     title: 'Nulla ut erat id mauris vulputate elementum.',
-    post_url: 'http://facebook.com',
+    content: 'Nulla ut erat id mauris vulputate elementum',
     user_id: 6
   },
 ];
