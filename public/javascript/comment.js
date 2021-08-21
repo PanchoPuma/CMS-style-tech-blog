dde232aee0df74255d88119bbdb38190b80e5dae
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-21 15:16:59 -0700

    working on the publicjs files

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,1 +1,31 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+  
+    const comment_textarea = document.querySelector('textarea[name="comment_textarea"]').value.trim();
+  
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_textarea) {
+        const response = await fetch('/api/comments', {
+          method: 'POST',
+          body: JSON.stringify({
+            post_id,
+            comment_textarea
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          alert(response.statusText);
+        }
+      }
+  }
+  
+  document.querySelector('.comment').addEventListener('submit', commentFormHandler);
 
