db834691175c69b43929eb95aee6b964d4ae30ea
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-21 19:52:50 -0700

    updted ports

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const hbs = exphbs.create({ helpers });
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
